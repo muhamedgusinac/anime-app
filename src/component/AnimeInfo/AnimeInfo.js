91d6354ec5c62ffc021b10ad32615c087ca83006
muhamedgusinac <muhamed.gusinac234@gmail.com>
2023-08-07 20:19:46 +0200

    my commit example

---
+++ src/component/AnimeInfo/AnimeInfo.js
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const AnimeInfo = () => {
+  const [animeData, setAnimeData] = useState([]);
+
+  const fetchAnimeData = async () => {
+    try {
+      const response = await axios.get('https://api.anime_app.com/anime');
+      setAnimeData(response.data);
+    } catch (error) {
+      console.error('Greška pri dohvatanju podataka o animeima:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchAnimeData();
+  }, []);
+
+  return (
+    <div>
+      <h2>Informacije o animeima</h2>
+      <ul>
+        {animeData.map((anime) => (
+          <li key={anime.id}>
+            {anime.title} - {anime.genre}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default AnimeInfo;
