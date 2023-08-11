4c62602615ba4d0f7aea99490c97d494eb4e3c1d
Krepak05 <hamzazukovic2@gmail.com>
2023-08-11 12:21:11 +0200

    spojen main branch (Ono sto je valjalo) sa branchom koji radi

---
+++ src/Components/Api/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import AnimeInfo from '.AnimeInfo';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Anime Aplikacija</h1>
+      <AnimeInfo />
+    </div>
+  );
+};
+
+export default App;
